6a24a9a08787fd15bebbed1e914920e618c55eb9
Svring <lxf83553899@163.com>
2022-05-05 16:06:50 +0800

    01

--- Training/PYTHON/avatar.py
+++ Training/PYTHON/avatar.py
@@ -1,44 +1,14 @@
-def improve(update, close, guess = 1):
-    ''' 
-    iterate the answer with the method
-
-    update:the method used to iterate value
-    close:the tolerance target
-    '''
-    while not close(guess):
-        guess = update(guess)
-    return guess
-
-def approx_eq(x, y, tolerance = 1e-10):
-    ''' judge if the tolerance meet the target '''
-    return abs(x - y) < tolerance
-
-def newton_update(f, df):
-    ''' 
-    iterate the function get in
-
-    return the next point
-    '''
-    def update(x):
-        return x - f(x) / df(x)
-    return update
-
-def find_zero(f, df):
-    ''' core function '''
-    def near_zero(x):
-        return approx_eq(f(x), 0)
-    return improve(newton_update(f, df), near_zero)
-
-def power(x, n):
-    ''' return the n'th power of x '''
-    product, k = 1, 0
-    while k < n:
-        product, k = product * x, k + 1
-    return product
-
-def nth_root_of_a(n, a):
-    def f(x):
-        return power(x, n) - a
-    def df(x):
-        return n * power(x, n - 1)
-    return find_zero(f, df)
+while score0 < goal and score1 < goal:
+        if who == 0:
+            num_rolls = strategy0(score0, score1)
+            addition = take_turn(num_rolls, score1, dice)
+            score0 += addition
+            if is_swap(score0, score1):
+                score0, score1 = score1, score0
+        else:
+            num_rolls = strategy1(score1, score0)
+            addition = take_turn(num_rolls, score0, dice)
+            score1 += addition
+            if is_swap(score1, score0):
+                score0, score1 = score1, score0
+        who = other(who)
